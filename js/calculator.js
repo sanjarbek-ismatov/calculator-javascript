62d1e0467f749aa41b37ae593003fa3692cf4b84
uzweb-code <ismatovsanjarbek@yandex.ru>
2022-04-22 18:58:16 +0500

    Update calculator.js

--- js/calculator.js
+++ js/calculator.js
@@ -1,70 +1,113 @@
 "use strict";
-/** @type {!Element} */
-var input = document.getElementsByTagName("input")[0];
-/** @type {!NodeList<Element>} */
-var button = document.getElementsByClassName("button");
-/** @type {boolean} */
-var equals = false;
-/** @type {boolean} */
-var _iteratorNormalCompletion = true;
-/** @type {boolean} */
-var _didIteratorError = false;
-var _iteratorError = undefined;
-try {
-  /**
-   * @return {undefined}
-   */
-  var _loop = function _loop$jscomp$1$jscomp$0() {
-    var i$jscomp$3$jscomp$0 = _step.value;
-    i$jscomp$3$jscomp$0.addEventListener("click", function () {
-      if (equals) {
-        /** @type {string} */
-        input.value = "";
-        /** @type {boolean} */
-        equals = false;
-      }
-      if (i$jscomp$3$jscomp$0.value === "=") {
-        if (input.value) {
-          /** @type {string} */
-          input.value = input.value + " = " + eval(input.value);
-          /** @type {boolean} */
-          equals = true;
-        }
-      } else {
-        input.value += i$jscomp$3$jscomp$0.value;
-      }
-      switch (i$jscomp$3$jscomp$0.value) {
-        case "c":
-          /** @type {string} */
-          input.value = "";
-          break;
-        case "sqrt":
-          /** @type {number} */
-          input.value = Math.sqrt(parseInt(input.value));
-      }
-    });
+var _0x1d5ed4 = _0x53c6;
+(function (_0x2dc781, _0x4b5bae) {
+  var _0x3036dc = _0x53c6,
+    _0x26276f = _0x2dc781();
+  while (!![]) {
+    try {
+      var _0x3823bc =
+        parseInt(_0x3036dc(0xfe)) / 0x1 +
+        -parseInt(_0x3036dc(0xfd)) / 0x2 +
+        parseInt(_0x3036dc(0xfc)) / 0x3 +
+        -parseInt(_0x3036dc(0xf0)) / 0x4 +
+        (parseInt(_0x3036dc(0xf3)) / 0x5) * (parseInt(_0x3036dc(0xed)) / 0x6) +
+        (parseInt(_0x3036dc(0xf1)) / 0x7) * (-parseInt(_0x3036dc(0xef)) / 0x8) +
+        parseInt(_0x3036dc(0xec)) / 0x9;
+      if (_0x3823bc === _0x4b5bae) break;
+      else _0x26276f["push"](_0x26276f["shift"]());
+    } catch (_0xb2802e) {
+      _0x26276f["push"](_0x26276f["shift"]());
+    }
+  }
+})(_0x5322, 0xc1e20);
+function _0x5322() {
+  var _0x4138ae = [
+    "984134IoEklv",
+    "1458029omdFQJ",
+    "iterator",
+    "14884560xGXfYY",
+    "1122ExkNFG",
+    "next",
+    "252376jIcXPW",
+    "4102872UMbnWB",
+    "343eMVIOR",
+    "done",
+    "7990BKmLXW",
+    "\x20=\x20",
+    "value",
+    "addEventListener",
+    "sqrt",
+    "click",
+    "getElementsByClassName",
+    "return",
+    "button",
+    "1341111JzoUid",
+  ];
+  _0x5322 = function () {
+    return _0x4138ae;
   };
-  var _iterator = button[Symbol.iterator]();
-  var _step;
+  return _0x5322();
+}
+var input = document["getElementsByTagName"]("input")[0x0],
+  button = document[_0x1d5ed4(0xf9)](_0x1d5ed4(0xfb)),
+  equals = ![],
+  _iteratorNormalCompletion = !![],
+  _didIteratorError = ![],
+  _iteratorError = undefined;
+function _0x53c6(_0x1d636e, _0x43bede) {
+  var _0x5322d5 = _0x5322();
+  return (
+    (_0x53c6 = function (_0x53c6dc, _0x3cc2b7) {
+      _0x53c6dc = _0x53c6dc - 0xeb;
+      var _0x5366c1 = _0x5322d5[_0x53c6dc];
+      return _0x5366c1;
+    }),
+    _0x53c6(_0x1d636e, _0x43bede)
+  );
+}
+try {
+  var _loop = function _loop$jscomp$1$jscomp$0() {
+      var _0x3085dd = _0x1d5ed4,
+        _0x346796 = _step[_0x3085dd(0xf5)];
+      _0x346796[_0x3085dd(0xf6)](_0x3085dd(0xf8), function () {
+        var _0x4afbbf = _0x3085dd;
+        equals && ((input["value"] = ""), (equals = ![]));
+        _0x346796["value"] === "="
+          ? input["value"] &&
+            ((input[_0x4afbbf(0xf5)] =
+              input[_0x4afbbf(0xf5)] +
+              _0x4afbbf(0xf4) +
+              eval(input[_0x4afbbf(0xf5)])),
+            (equals = !![]))
+          : (input["value"] += _0x346796["value"]);
+        switch (_0x346796[_0x4afbbf(0xf5)]) {
+          case "c":
+            input[_0x4afbbf(0xf5)] = "";
+            break;
+          case "sqrt":
+            input["value"] = Math[_0x4afbbf(0xf7)](parseInt(input["value"]));
+        }
+      });
+    },
+    _iterator = button[Symbol[_0x1d5ed4(0xeb)]](),
+    _step;
   for (
     ;
-    !(_iteratorNormalCompletion = (_step = _iterator.next()).done);
-    _iteratorNormalCompletion = true
+    !(_iteratorNormalCompletion = (_step = _iterator[_0x1d5ed4(0xee)]())[
+      _0x1d5ed4(0xf2)
+    ]);
+    _iteratorNormalCompletion = !![]
   ) {
     _loop();
   }
-} catch (err) {
-  /** @type {boolean} */
-  _didIteratorError = true;
-  _iteratorError = err;
+} catch (_0x280863) {
+  (_didIteratorError = !![]), (_iteratorError = _0x280863);
 } finally {
   try {
-    if (!_iteratorNormalCompletion && _iterator.return) {
-      _iterator.return();
-    }
+    !_iteratorNormalCompletion &&
+      _iterator[_0x1d5ed4(0xfa)] &&
+      _iterator[_0x1d5ed4(0xfa)]();
   } finally {
-    if (_didIteratorError) {
-      throw _iteratorError;
-    }
+    if (_didIteratorError) throw _iteratorError;
   }
 }
